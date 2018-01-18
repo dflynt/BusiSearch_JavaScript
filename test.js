35d2b4e1c93638ce4271dfeb8273565fc7e8623c
dflynt <flyntdw@unc.edu>
2018-01-17 19:52:53 -0500

    beginning conversion to javascript

--- test.js
+++ test.js
@@ -1,1 +1,39 @@
-alert("hello world")
+function Search(){
+    var text = "";
+    var matches = [];
+    var noDupesMatches = [];
+}
+Search.prototype.stocks = function(){
+    var file = new XMLHttpRequest();
+    file.onreadystatechange = function() {
+        if(file.readyState === 4)
+        {
+            if(file.status === 200 || file.status == 0)
+            {
+                var allText = file.responseText;
+                console.log(allText);
+                return allText;
+            }
+        }
+        else{
+            console.log("false");
+        }
+    }
+    file.open("GET", "file:///Users/dannyflynt/BusiSearch_JavaScript/Stocks.txt"), true;
+    file.send();
+}
+Search.prototype.checkBusiness = function(line, index, name, counter) {
+        name = name.lower();
+        if(counter != 5) {
+            if(index == 0){
+
+            }
+        }
+}
+Search.prototype.analyze = function(){
+
+}
+Search.prototype.returnMatches = function() {return noDupesMatches;}
+
+var search = new Search();
+console.log(search.stocks());
