ca7bcd97c1a0a08d5e5f5fadc32f4be0c1ac51dc
daniel <flyntdw@unc.edu>
2018-01-19 03:14:22 -0500

    implemented working parser

--- test.js
+++ test.js
@@ -1,54 +1,101 @@
 var Search = {
-    stockListDict: {},
+    stocksDict: {},
     matches: [],
     noDupesMatches: [],
     webPageText:[],
 }
-function checkBusiness(line, index, name, counter) {
-    name = name.lower();
-    if(counter != 5) {
-        if(index == 0){
-        }
-    }
-}
-function analyze() {
-    for(var line = 0; line < Search.webPageText.length; line++) {
-        for(var word = Search.webPageText.length - 1; word >= 0; word--) {
-            line[word] = line[word].replace("\'s", ""); //parses noun possession
-            //checkBusiness(line in <p>, index of line, company name, counter)
-            checkBusiness(line, word, line[word], 0);
-        }
-    }
-}
-function returnMatches() {return noDupesMatches;}
 
-function onReadyStateChange() {
-    var stockList = this.responseText; //list of stocks from .txt file, separated by \n
-    stockList = stockList.split("\n");
+$(document).ready(function() {
 
-    for(var i = 0; i < stockList.length; i++) {
-        var stock = stockList[i].split(", "); //Key: Symbol  Value: Name
-        Search.stockListDict[stock[0]] = stock[1].replace("\n", "");
+    function onReadyStateChange() {
+        var stockList = this.responseText; //list of stocks from .txt file, separated by \n
+        stockList = stockList.split("\n");
+    
+        for(var i = 0; i < stockList.length; i++) {
+            var stock = stockList[i].split(", "); //Key: Symbol  Value: Name
+            Search.stocksDict[stock[0].trim()] = stock[1].replace("\n", "").trim();
+        }
+        console.log(Search.stocksDict);
+        analyze(); //Why does calling analyze() here work but not in onReadyHTMLStateChange() ???
     }
-    console.log(Search.stockListDict);
-}
-function onReadyHTMLStateChange(){
-    console.log("On ready html");
-    $(document).ready(function() {
-        $("p").each(function(index) {
-            Search.webPageText.push($(this).text().replace("\n", ""));
+
+    function onReadyHTMLStateChange(){
+        $(document).ready(function() {
+            $("p").each(function(index) {
+                Search.webPageText.push($(this).text());
+            });
         });
-    });
-    console.log(Search.webPageText);
-}
-var file = new XMLHttpRequest();
-file.addEventListener("load", onReadyStateChange);
-file.open("GET", "http://localhost:8080/Stocks.txt"), true;
-file.send();
+    }
+    
+    var file = new XMLHttpRequest();
+    file.addEventListener("load", onReadyStateChange);
+    file.open("GET", "http://localhost:8080/Stocks.txt"), true;
+    file.send();
+    
+    var textHTML = new XMLHttpRequest();
+    textHTML.addEventListener("load", onReadyHTMLStateChange);
+    textHTML.open("GET", "http://localhost:8080/htmlEx.html"), true;
+    textHTML.send();
 
-var textHTML = new XMLHttpRequest();
-textHTML.addEventListener("load", onReadyHTMLStateChange);
-textHTML.open("GET", "http://localhost:8080/htmlEx.html"), true;
-textHTML.send();
+    function checkBusiness(dict, line, index, name, counter) {
+        name = name.toLowerCase().trim();
+        if (counter != 5) {
+            if(index == 0){
+                for(var key in dict) {
+                    if(dict.hasOwnProperty(key)) {
+                        if(dict[key] === name) {
+                            Search.matches.push(name);
+                        }
+                    }
+                }
+            }
+            else if(checkBusiHelper(name)){
+                Search.matches.push(name);
+                //console.log(matches);
+                return;
+            }
+            else {
+                checkBusiness(dict, line, index - 1, line[index - 1] + " " + name, counter + 1);
+            }
+        }
+        else{
+            return null;
+        }
 
-analyze();
+        function checkBusiHelper(name) {
+            for(var k in dict) {
+                if(dict.hasOwnProperty(k)) {
+                    if(dict[k] === name) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    function analyze() {
+        Search.webPageText.forEach(function(line) { //for each line in a <p> tag
+            line = line.toLowerCase();
+            line = line.split(" "); //turn into a list to check each word
+            //console.log(line);
+            for(var index = line.length - 1; index >= 0; index--) { //for each word in the line
+                //console.log(line + " " + index + " " + line[index], 0);
+                line[index] = line[index].replace("\'s", "").trim(); //parses noun possession
+                //checkBusiness(line in <p>, index of line, company name, counter)
+                //console.log(line + " " + index + " " + line[index]);
+                checkBusiness(Search.stocksDict, line, index, line[index].trim(), 0);
+            } 
+        });
+        console.log(Search.matches)
+        for(var match in Search.matches) { //for each match found in the page
+            for(var stock in Search.stocksDict) { //for each stock in the stocksDict dictionary
+                if(match === Search.stocksDict[stock]) { //hey, it's an actual stock
+                    Search.noDupesMatches[stock] = match;
+                }
+            }   
+        }
+    }
+
+    function returnMatches() {return Search.noDupesMatches;}
+
+});
